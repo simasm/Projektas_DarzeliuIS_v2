7d85524ad7ae6766ba21a1b1ec9b91aff8a24f2f
vaidas <vaidas.bulovas52@gmail.com>
2022-02-01 12:16:13 +0200

    fixed bugs with text selecting on double click and mouse leave event

--- front/src/components/08CommonComponents/Dropdown.jsx
+++ front/src/components/08CommonComponents/Dropdown.jsx
@@ -4,62 +4,67 @@ import './../../App.css'
 import { useHistory } from "react-router-dom";
 
 
- function Dropdown() {
+function Dropdown() {
     let history = useHistory();
     const [isActive, setIsActive] = useState(false);
-    
+
 
     const options = [
-        {regType: 'Prašymas dėl registracijos į darželį',
-         path: '/prasymai/registracija'}, 
+        {
+            regType: 'Prašymas dėl registracijos į darželį',
+            path: '/prasymai/registracija'
+        },
 
-         {regType: 'Prašymas dėl kompensacijos',
-         path: '/prasymai/kompensacija'},
-         
-         ];
+        {
+            regType: 'Prašymas dėl kompensacijos',
+            path: '/prasymai/kompensacija'
+        },
+
+    ];
 
 
-        const handleSetActive = () => {
-             setIsActive(!isActive);
-         }
+    const handleSetActive = (e) => {
+        e.preventDefault();
+        setIsActive(!isActive);
+    }
 
-  return(
+    return (
 
-    <div className='dropdown container' 
-    onMouseLeave={handleSetActive}>
+        <div className='dropdown container'
+            onMouseLeave={() => setIsActive(false)}>
 
-        <div className="nav-link dropdown-btn"
-         id="navUserNewApplication" 
-         
-         onClick={handleSetActive}>
-            Sukurti prašymą
-            
-        </div>
-        {isActive && (
-            <div className="dropdown-content"
-            >
-            
-                {options.map(option => (
-                         <div key={option.regType} onClick={() => {
-                        setIsActive(false);
-                        history.push(option.path)
-                        
-                }}
-                        className="dropdown-item">
-                            {option.regType}
-                            
-                         </div>
-                ))}
-               
-            
+            <div className="nav-link dropdown-btn btnnoselect"
+                id="navUserNewApplication"
+
+                onClick={handleSetActive}>
+                Sukurti prašymą
+
             </div>
-        )}
-            
+            {isActive && (
+                <div className="dropdown-content"
+                >
+
+                    {options.map(option => (
+                        <div key={option.regType} onClick={() => {
+                            setIsActive(false);
+                            history.push(option.path)
+
+                        }}
+                            className="dropdown-item">
+                            {option.regType}
+
+                        </div>
+                    ))}
+
+
+                </div>
+            )}
 
 
 
-    </div>
-            ) 
+
+        </div>
+    )
 }
 
 
