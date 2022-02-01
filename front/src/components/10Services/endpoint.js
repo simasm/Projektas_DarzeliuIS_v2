f4ae97c6e6726b65363941fc6c2d45a39c822443
simasm <simonas.mark@gmail.com>
2022-02-01 13:23:06 +0200

    endpoint for tomcat

--- front/src/components/10Services/endpoint.js
+++ front/src/components/10Services/endpoint.js
@@ -1,5 +1,5 @@
 //prieš yarn build užkomentuoti localhost ir atkomentuoti šitą endpoint:
-apiEndpoint = process.env.PUBLIC_URL;
+const apiEndpoint = process.env.PUBLIC_URL;
 
 //dirbant lokaliai per yarn start paleidus react app per localhost:3000
 //const apiEndpoint = "http://localhost:8080"
