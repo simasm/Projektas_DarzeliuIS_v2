f338a6c9d2c8ea690465eb5687ae82d245ecc7ac
simasm <simonas.mark@gmail.com>
2022-02-07 15:36:23 +0200

    delete by child code

--- back/src/test/java/it/akademija/compensation/CompensationControllerTest.java
+++ back/src/test/java/it/akademija/compensation/CompensationControllerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
@@ -63,9 +62,11 @@ public class CompensationControllerTest {
 	void controllerRespondsWith201And400() {
 	
 		
-		assertEquals(HttpStatus.CREATED, controller.createNewCompensationApplication(data)
+		assertEquals(HttpStatus.CREATED,
+				 controller.createNewCompensationApplication(data)
 				.getStatusCode());
-		assertEquals(HttpStatus.BAD_REQUEST, controller.createNewCompensationApplication(data)
+		assertEquals(HttpStatus.BAD_REQUEST, 
+				 controller.createNewCompensationApplication(data)
 				.getStatusCode());
 				
 	}
@@ -84,14 +85,29 @@ public class CompensationControllerTest {
 	@Order(4)
 	@WithMockUser(username="test1@test.lt", roles = { "MANAGER"})
 	void managerCanRetrieveCompensationApplications() {
-		assertEquals(controller.getAllCopensationApplications().getStatusCode(),
-				HttpStatus.OK);
-		
-		
+		assertEquals(HttpStatus.OK,
+				controller.getAllCopensationApplications().getStatusCode()
+				);
+ 
 	}
 	
+	  
 	@Test
 	@Order(5)
+	@WithMockUser(username="test@test.lt", roles = { "MANAGER", "ADMIN"})
+	void getsCompensationApplicationByChildCode() {
+		Compensation compensation = null;
+		compensation = controller.getCompensationApplicationByChildCode(
+				data.getChildInfo().getPersonalID()).getBody();
+		assertNotNull(compensation);
+		assertEquals(data.getChildInfo().getPersonalID(),
+				compensation.getChildPersonalCode());
+				
+	}
+	 
+	
+	@Test
+	@Order(6)
 	@WithMockUser(username="test@test.lt", roles = { "MANAGER"})
 	void managerCanDeleteCompensationApplicationByChildCode() {
 		//assertEquals(controller.deleteCompensationApplicationByUsername(null))
@@ -100,21 +116,23 @@ public class CompensationControllerTest {
 		int size = controller.getAllCopensationApplications()
 				.getBody()
 				.size();
-
-		
+ 
 		assertTrue(size > 0 );
-	 
-		
+ 
 		controller.deleteCompensationApplicationByChildCode(
-				data.getChildInfo().getPersonalID());
+				data.getChildInfo()
+					.getPersonalID());
 		
 		assertTrue(controller.getAllCopensationApplications()
 				.getBody()
 				.size() < size );
  
-		
-		
 	}
 		
- 
+
+	
+	/* to do  
+	 *      deleteCompensationApplicationByUsername
+	 *      getCompensationApplicationForUser
+	 */    
 }
