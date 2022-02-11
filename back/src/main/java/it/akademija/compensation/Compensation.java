056c4edf9212e5d999c97ee47caca3019236f137
Tomas <m21tomas@gmail.com>
2022-02-11 13:51:01 +0200

    Compensation form fields validation regex disabled on backend

--- back/src/main/java/it/akademija/compensation/Compensation.java
+++ back/src/main/java/it/akademija/compensation/Compensation.java
@@ -48,8 +48,8 @@ public class Compensation {
 	//@Pattern(regexp = "^\\p{L}+(?: \\p{L}+)*$")
 	private String childSurname;
 
-	@Pattern(regexp = "^(?!\\s*$)[0-9\\s]{11}$|")
-	//@NotEmpty(message = "Kodas privalomas")
+	//@Pattern(regexp = "^(?!\\s*$)[0-9\\s]{11}$|")
+	@NotEmpty(message = "Kodas privalomas")
  	private String childPersonalCode;
 
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -59,37 +59,7 @@ public class Compensation {
 	
 	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.DETACH })
 	@JoinColumn(name = "user_id")
-	private User mainGuardian;
-	
- 
-	
-	public Compensation(
-			@NotEmpty(message = "Vardas privalomas!") @Size(min = 2, max = 70) String childName,
-			@NotEmpty(message = "Pavardė privaloma!") @Size(min = 2, max = 70) String childSurname,
-			@Pattern(regexp = "^(?!\\s*$)[0-9\\s]{11}$|") String childPersonalCode, LocalDate childBirthdate,
-			User mainGuardian, String kindergartenId,
-			@NotBlank(message = "Pavadinimas privalomas") String kindergartenName,
-			@NotBlank(message = "Adresas privalomas") String kindergartenAddress,
-			@NotBlank(message = "Telefonas privalomas") String kindergartenPhoneNumber,
-			@Email @NotEmpty(message = "El. paštas privalomas") String kindergartenEmail,
-			@NotBlank(message = "Banko pavadinimas privalomas") String kindergartenBankName,
-			@NotBlank(message = "Banko saskaitos numeris privalomas") String kindergartenBankAccountNumber,
-			@NotBlank(message = "Banko kodas privalomas") String kindergartenBankCode) {
-		super();
-		this.childName = childName;
-		this.childSurname = childSurname;
-		this.childPersonalCode = childPersonalCode;
-		this.childBirthdate = childBirthdate;
-		this.mainGuardian = mainGuardian;
-		this.kindergartenId = kindergartenId;
-		this.kindergartenName = kindergartenName;
-		this.kindergartenAddress = kindergartenAddress;
-		this.kindergartenPhoneNumber = kindergartenPhoneNumber;
-		this.kindergartenEmail = kindergartenEmail;
-		this.kindergartenBankName = kindergartenBankName;
-		this.kindergartenBankAccountNumber = kindergartenBankAccountNumber;
-		this.kindergartenBankCode = kindergartenBankCode;
-	}
+	private User mainGuardian;	
 
 	//kindergarten ifno
 	 
@@ -118,7 +88,6 @@ public class Compensation {
 	//@Pattern(regexp = "\\S[\\s\\S]{2,49}")
 	private String kindergartenBankName;
 
-	
 	@Column
 	@NotBlank(message = "Banko saskaitos numeris privalomas")
 	private String kindergartenBankAccountNumber;
@@ -130,8 +99,35 @@ public class Compensation {
 	public Compensation() {
 		
 	}
-
 	
+	
+
+	public Compensation(@NotEmpty(message = "Vardas privalomas!") @Size(min = 2, max = 70) String childName,
+			@NotEmpty(message = "Pavardė privaloma!") @Size(min = 2, max = 70) String childSurname,
+			@NotEmpty(message = "Kodas privalomas") String childPersonalCode, LocalDate childBirthdate,
+			User mainGuardian, String kindergartenId,
+			@NotBlank(message = "Pavadinimas privalomas") String kindergartenName,
+			@NotBlank(message = "Adresas privalomas") String kindergartenAddress,
+			@NotBlank(message = "Telefonas privalomas") String kindergartenPhoneNumber,
+			@Email @NotEmpty(message = "El. paštas privalomas") String kindergartenEmail,
+			@NotBlank(message = "Banko pavadinimas privalomas") String kindergartenBankName,
+			@NotBlank(message = "Banko saskaitos numeris privalomas") String kindergartenBankAccountNumber,
+			@NotBlank(message = "Banko kodas privalomas") String kindergartenBankCode) {
+		super();
+		this.childName = childName;
+		this.childSurname = childSurname;
+		this.childPersonalCode = childPersonalCode;
+		this.childBirthdate = childBirthdate;
+		this.mainGuardian = mainGuardian;
+		this.kindergartenId = kindergartenId;
+		this.kindergartenName = kindergartenName;
+		this.kindergartenAddress = kindergartenAddress;
+		this.kindergartenPhoneNumber = kindergartenPhoneNumber;
+		this.kindergartenEmail = kindergartenEmail;
+		this.kindergartenBankName = kindergartenBankName;
+		this.kindergartenBankAccountNumber = kindergartenBankAccountNumber;
+		this.kindergartenBankCode = kindergartenBankCode;
+	}
 
 
 
@@ -139,85 +135,57 @@ public class Compensation {
 		return submittedAt;
 	}
 
-
-
 	public void setSubmittedAt() {
 		this.submittedAt = LocalDate.now();
 	}
-
-
 
 	public String getChildName() {
 		return childName;
 	}
 
-
-
 	public void setChildName(String childName) {
 		this.childName = childName;
 	}
-
-
 
 	public String getChildSurname() {
 		return childSurname;
 	}
 
-
-
 	public void setChildSurname(String childSurname) {
 		this.childSurname = childSurname;
 	}
-
-
 
 	public String getChildPersonalCode() {
 		return childPersonalCode;
 	}
 
-
-
 	public void setChildPersonalCode(String childPersonalCode) {
 		this.childPersonalCode = childPersonalCode;
 	}
-
-
 
 	public LocalDate getChildBirthdate() {
 		return childBirthdate;
 	}
 
-
-
 	public void setChildBirthdate(LocalDate childBirthdate) {
 		this.childBirthdate = childBirthdate;
 	}
-
-
 
 	public User getMainGuardian() {
 		return mainGuardian;
 	}
 
-
-
 	public void setMainGuardian(User mainGuardian) {
 		this.mainGuardian = mainGuardian;
 	}
-
-
 
 	public String getKindergartenId() {
 		return kindergartenId;
 	}
 
-
-
 	public void setKindergartenId(String kindergartenId) {
 		this.kindergartenId = kindergartenId;
 	}
-
-
 
 	public String getKindergartenName() {
 		return kindergartenName;
@@ -229,55 +197,37 @@ public class Compensation {
 		this.kindergartenName = kindergartenName;
 	}
 
-
-
 	public String getKindergartenAddress() {
 		return kindergartenAddress;
 	}
-
-
 
 	public void setKindergartenAddress(String kindergartenAddress) {
 		this.kindergartenAddress = kindergartenAddress;
 	}
 
-
-
 	public String getKindergartenPhoneNumber() {
 		return kindergartenPhoneNumber;
 	}
-
-
 
 	public void setKindergartenPhoneNumber(String kindergartenPhoneNumber) {
 		this.kindergartenPhoneNumber = kindergartenPhoneNumber;
 	}
 
-
-
 	public String getKindergartenEmail() {
 		return kindergartenEmail;
 	}
-
-
 
 	public void setKindergartenEmail(String kindergartenEmail) {
 		this.kindergartenEmail = kindergartenEmail;
 	}
 
-
-
 	public String getKindergartenBankName() {
 		return kindergartenBankName;
 	}
 
-
-
 	public void setKindergartenBankName(String kindergartenBankName) {
 		this.kindergartenBankName = kindergartenBankName;
 	}
-
-
 
 	public String getKindergartenBankAccountNumber() {
 		return kindergartenBankAccountNumber;
